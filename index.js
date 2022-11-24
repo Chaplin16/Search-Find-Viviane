a288b37c6d114d85231a26f60fc1e61b2766936b
Chaplin16 <chaplin16@orange.fr>
2022-11-24 12:34:31 +0100

    responsive and card in the every game

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// const games = [...document.querySelectorAll(".game")];
-// const answers = document.querySelectorAll(".answer");
-// let currentGame = 1;
-// const otherChoice = document.querySelectorAll(".photo");
+const games = [...document.querySelectorAll(".game")];
+const answers = document.querySelectorAll(".answer");
+let currentGame = 1;
+const otherChoice = document.querySelectorAll(".photo");
 
-// for (let i = 0; i < otherChoice.length; i++) {
-//     otherChoice[i].addEventListener('click', () => {
-//         alert(`Dommage 😞, essaye encore! `)   
-//     }) 
-//  }
+for (let i = 0; i < otherChoice.length; i++) {
+    otherChoice[i].addEventListener('click', () => {
+        alert(`Dommage 😞, essaye encore! `)   
+    }) 
+ }
 
-// answers.forEach((answer) => {
-//   answer.addEventListener("click", () => {
-//     alert(`Bravo, c'est la bonne image 👍`);
-//     if (currentGame === games.length) return;
-//     games[currentGame].style.display = "block";
-//     currentGame++;
-//   });
-// });
+answers.forEach((answer) => {
+  answer.addEventListener("click", () => {
+    alert(`Bravo, c'est la bonne image 👍`);
+    if (currentGame === games.length) return;
+    games[currentGame].style.display = "block";
+    currentGame++;
+  });
+});
